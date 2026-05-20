cbb0ee3c0adb85e1dc5b4dcfd58cb587a8f42376
Dort <rasmuswp2210@gmail.com>
2026-05-20 11:08:42 +0200

    Fikset modellen så vi har email i stedet for username, samt kommenteret koden.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -22,7 +22,11 @@ public class AuthController : ControllerBase
         _logger = logger;
     }
     
-    private string GenerateJwtToken(string username)
+    // Her genereres en JWT token, som bestemmer hvor meget der gives adgang til og hvor længe.
+    // Vi har en Issuer som er den der udsteder tokenen.
+    // Vores Secret skal være LANG for at virke, denne hashes så med SHA256.
+    // Tokenen er knyttet til email'en.
+    private string GenerateJwtToken(string email)
     {
         var securityKey =
             new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Secret"]));
@@ -30,7 +34,7 @@ public class AuthController : ControllerBase
             new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
         var claims = new[]
         {
-            new Claim(ClaimTypes.NameIdentifier, username)
+            new Claim(ClaimTypes.NameIdentifier, email)
         };
         var token = new JwtSecurityToken(
             _config["Issuer"],
@@ -41,15 +45,32 @@ public class AuthController : ControllerBase
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
     
+    // Login endpoint, der tager en email og et password som parametre
+    // og sender en token tilbage med adgang til sider der kræver authorisation.
     [AllowAnonymous]
     [HttpPost("login")]
     public async Task<IActionResult> Login([FromBody] LoginModel login)
     {
-        if (login.Username == "haavy_user" && login.Password == "aaakodeord")
+        // Eksempel på brugerdata
+        var users = new List<LoginModel>
         {
-            var token = GenerateJwtToken(login.Username);
+            new LoginModel { Email = "test@test.com", Password = "1234" },
+            new LoginModel { Email = "user@test.com", Password = "abcd" }
+        };
+        
+        // Her tages brugerinput og der tjekkes om dette passer på de brugere der findes.
+        var user = users.FirstOrDefault(u =>
+            u.Email == login.Email &&
+            u.Password == login.Password);
+        
+        // Hvis user ikke er null får man en token.
+        // Hvis user er null eller forkert får man ikke en token.
+        if (user != null)
+        {
+            var token = GenerateJwtToken(user.Email);
             return Ok(new { token });
         }
+
         return Unauthorized();
     }
     
